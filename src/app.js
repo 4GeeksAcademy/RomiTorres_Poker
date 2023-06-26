8a2cd476672a0b927ff4d8a506598e8cedbc118d
Romina Verónica Torres <110548507+RomiTorres@users.noreply.github.com>
2023-06-26 19:47:39 +0000

    Proyecto parcial

--- src/app.js
+++ src/app.js
@@ -7,5 +7,46 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  function aleatorioSuit() {
+    let suit = ["♦", "♥", "♠", "♣"];
+    let aleatorioPalos = Math.floor(Math.random() * suit.length);
+
+    return suit[aleatorioPalos];
+  }
+
+  function aleatorioNumber() {
+    let number = [
+      "A",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let aleatoriosNumeros = Math.floor(Math.random() * number.length);
+    return number[aleatoriosNumeros];
+  }
+
+  let resultSuit = aleatorioSuit();
+
+  let suitCard = document.querySelectorAll(".suit");
+  suitCard.forEach(item => {
+    item.innerHTML = resultSuit;
+  });
+
+  let resultNumber = aleatorioNumber();
+
+  let numberCard = document.querySelector(".number");
+  numberCard.innerHTML = resultNumber;
+  console.log(aleatorioSuit());
+  console.log(aleatorioNumber());
   console.log("Hello Rigo from the console!");
 };
